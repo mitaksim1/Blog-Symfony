14cf1d9c861fb3453daf88214a2d0c799ad6813f
mitaksim1 <miriamt.simonnet@gmail.com>
2021-04-05 23:05:12 +0200

    Symfony-1 : Création entité, controleur, fixtures, template, paramConverter, injection des dépendances

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(ArticleRepository $repo): Response
+    {
+        $articles = $repo->findAll();
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+       
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue ici les amis !",
+            'age' => 31
+        ]);
+    }
+
+    /**
+     *@route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article)
+    {
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+}
