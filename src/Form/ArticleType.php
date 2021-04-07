cd89224477bc08f79b4094d10529dcde200482da
mitaksim1 <miriamt.simonnet@gmail.com>
2021-04-07 12:44:40 +0200

    Ajout du champs category dans le formulaire plus changements dans la vue create

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ArticleType extends AbstractType
 {
@@ -13,6 +15,13 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title')
+            // On avait pas de catégorie dans le formulaire, comme il s'agit d'une relation il faut utiliser la classe EntityType
+            // où on va relier le champs category avec l'entité category
+            // Une fois le champs rajouté on oublie pas de changer la vue create
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
             ->add('content')
             ->add('image')
         ;
